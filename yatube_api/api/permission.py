f631e86cf337dd49969f6b5cf6a0f690959f041f
Николай Судаков <sudakovkoli@mail.ru>
2022-10-23 04:39:23 +0900

    Dsdsd

--- yatube_api/api/permission.py
+++ yatube_api/api/permission.py
@@ -1,8 +1,11 @@
 from rest_framework import permissions
 
+
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    """Разрешения только для авторов то есть delete  patch."""
 
     def has_object_permission(self, request, view, obj):
+        """."""
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
